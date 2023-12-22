cdf1077e0b7130e62ef43a14c28c83e7c109ac07
Olivier3456 <olivier.larrey@gmail.com>
2023-12-22 09:24:41 +0100

    Setup VR, works fine in Editor with Quest Link

--- Assets/Scripts/SpawnSpider.cs
+++ Assets/Scripts/SpawnSpider.cs
@@ -1,23 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SpawnSpider : MonoBehaviour
 {
     [SerializeField] private GameObject prefab;
-    
 
-    void Update()
+    [SerializeField] private InputActionReference spawnSpider;
+
+
+    private void OnEnable()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GameObject obj = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+        spawnSpider.action.performed += SpawnASpider;
+        spawnSpider.action.Enable();
+    }
 
-            if (obj.TryGetComponent(out Spider spider))
-            {
-                spider.SetSpiderSize(0.05f);
-                spider.SetPosition(transform.position);
-            }
+    private void SpawnASpider(InputAction.CallbackContext obj)
+    {
+        GameObject go = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+
+        if (go.TryGetComponent(out Spider spider))
+        {
+            spider.SetSpiderSize(0.05f);
+            spider.SetPosition(transform.position);
         }
     }
+
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+
+    //    {
+    //        GameObject obj = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+
+    //        if (obj.TryGetComponent(out Spider spider))
+    //        {
+    //            spider.SetSpiderSize(0.05f);
+    //            spider.SetPosition(transform.position);
+    //        }
+    //    }
+    //}
 }
